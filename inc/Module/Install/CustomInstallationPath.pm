73034aa4b2122f5447ce17c01cc17e2511998995
David Coppit <david@coppit.org>
2004-09-16 05:43:43 +0000

    Import of DCOPPIT/Mail-Mbox-MessageParser-1.21.0 from CPAN.
    gitpan-cpan-distribution: Mail-Mbox-MessageParser
    gitpan-cpan-version:      1.21.0
    gitpan-cpan-path:         DCOPPIT/Mail-Mbox-MessageParser-1.21.0.tar.gz
    gitpan-cpan-author:       DCOPPIT
    gitpan-cpan-maturity:     released

---
+++ inc/Module/Install/CustomInstallationPath.pm
@@ -0,0 +1,47 @@
+#line 1 "inc/Module/Install/CustomInstallationPath.pm - /Library/Perl/5.8.1/Module/Install/CustomInstallationPath.pm"
+package Module::Install::CustomInstallationPath;
+
+use strict;
+use File::HomeDir;
+
+use vars qw( @ISA $VERSION );
+
+use Module::Install::Base;
+@ISA = qw( Module::Install::Base );
+
+$VERSION = '0.10.1';
+
+# ---------------------------------------------------------------------------
+
+sub Check_Custom_Installation
+{
+  my $self = shift;
+
+  return if (grep {/^PREFIX=/} @ARGV) || (grep {/^INSTALLDIRS=/} @ARGV);
+
+  my $install_location = $self->prompt(
+    "Choose your installation type:\n[1] normal Perl locations\n" .
+    "[2] custom locations\n=>" => '1');
+
+  if ($install_location eq '2')
+  {
+    my $home = home();
+
+    die "Your home directory could not be determined. Aborting."
+      unless defined $home;
+
+    print "\n","-"x78,"\n\n";
+
+    my $prefix = $self->prompt(
+      "What PREFIX should I use?\n=>" => $home);
+
+    push @ARGV,"PREFIX=$prefix";
+  }
+}
+
+1;
+
+# ---------------------------------------------------------------------------
+
+#line 94
+
